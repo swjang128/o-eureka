15bcd32a18429de06309f6034ea0537767129761
seongwook-jang <swjang128@humuson.com>
2022-08-10 17:59:49 +0900

    Eureka Server 생성

---
+++ src/main/java/o/eureka/OEurekaApplication.java
@@ -0,0 +1,13 @@
+package o.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/o/eureka/OEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package o.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
